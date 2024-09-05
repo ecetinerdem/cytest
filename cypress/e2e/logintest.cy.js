83d7e0a209acf474de7afbf075c454bef309066f
Cetin Erdem <cetoerdem1@gmail.com>
2024-09-05 21:17:49 +0400

    CypressTest

--- cypress/e2e/logintest.cy.js
+++ cypress/e2e/logintest.cy.js
@@ -1,5 +1,48 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("login", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("hatalı eposta girişi yapınca buton disabled olmalı", () => {
+    cy.get('input[name="password"]').type("1234");
+    cy.get('input[name="terms"]').click();
+    cy.get('input[name="email"]').type("asd@adfacom");
+    const submit = cy.get('[data-cy="login-submit"]');
+    submit.should("be.disabled");
+  });
+  it("hatalı kısa şifre girişi yapınca buton disabled olmalı", () => {
+    cy.get('input[name="terms"]').click();
+    cy.get('input[name="email"]').type("asd@adfa.com");
+    cy.get('input[name="password"]').type("123");
+    const submit = cy.get('[data-cy="login-submit"]');
+    submit.should("be.disabled");
+  });
+
+  it("hatalı giriş yapınca error sayfasına yönlendiriyor", () => {
+    cy.get('input[name="email"]').type("asd@adfa.com");
+    cy.get('input[name="password"]').type("1234!asD");
+    cy.get('input[name="terms"]').click();
+    const submit = cy.get('[data-cy="login-submit"]');
+    submit.should("not.be.disabled");
+    submit.click();
+
+    cy.url().should("eq", "http://localhost:5173/");
+  });
+});
